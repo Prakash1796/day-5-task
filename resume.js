74d765ef8f7ab1fc07378cc2bd6c947d9b340dd6
Prakash1796 <130633940+Prakash1796@users.noreply.github.com>
2023-12-27 18:46:54 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,58 @@
+{
+    "basics": {
+        "name": "PRAKASH K",
+            "email": "edgeprakash1796@gmail.com",
+                "phone": "7010169614",
+                    "degree": "B.TECH CSE",
+                        "location": {
+            "address": "NO.59/95 MANAGAMMAL GARDEN ",
+                "postalCode": "6000081,
+            "city": "CHENNAI",
+                "countryCode": "INDIA",
+      },
+        "profiles": [
+            {
+                "network": " Instagram ",
+                "username": "fafprakash17",
+            }
+        ]
+    },
+
+
+    "education": [
+        {
+            "institution": "MGR University",
+            "area": "B.TECH",
+            "studyType": "FULL TIME",
+            "startDate": " 2014-17-7 ",
+            "endDate": " 2018-5-12 format",
+            "gpa": "6.50",
+            "courses": ["B.TECH CSE"]
+        }
+    ],
+
+
+        "skills": [
+            {
+                "name": "full stack",
+                "level": "average",
+                "keywords": [
+                    "java,javascript,c++,phython"
+                ]
+            }
+        ],
+            "languages": [
+                {
+                    "language": "tamil,english",
+                    "fluency": "tamil"
+                }
+            ],
+                "interests": [
+                    {
+                        "name": "sports",
+                        "keywords": [
+                            "cricket"
+                        ]
+                    }
+                ]
+}
